cbfbdd9ecdd362be4b09ff9518e3429566ba21f5
unknown <balijapallisatyasaijava1996@gmail.com>
2023-01-03 15:08:02 +0530

    first commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,11 @@
-import React from 'react'
+import React from "react";
 
 function Contact() {
   return (
     <div>
-      <h2>Contact app</h2>
-      
+      <h2>Contact</h2>
     </div>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
